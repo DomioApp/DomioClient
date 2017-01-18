c7cb9ec0f79f0ff9f2a95d8aff59ac164fa8ec53
Sergei Basharov <sergey@basharov.net>
2017-01-18 08:19:55 +0200

    restructuring

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_NAME=domio_public
+PROJECT_NAME=domio_client
 
 #buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S'`
 buildstamp=`date -u '+%s'`
@@ -18,34 +18,5 @@ echo "  Version:    ${version}"
 echo ---------------------------
 echo
 
-platform='unknown'
-unamestr=`uname`
-if [[ "$unamestr" == 'CYGWIN_NT-10.0' ]]; then
-   platform='cygwin'
-elif [[ "$unamestr" == 'FreeBSD' ]]; then
-   platform='freebsd'
-fi
-
-
-if [ $platform == "cygwin" ]
-    then
-        echo Compiling for Windows...
-        echo Output folder: /usr/local/bin/${PROJECT_NAME}_win.exe
-        export GOARCH=amd64
-        export GOOS=windows
-        go build -o /usr/local/bin/${PROJECT_NAME}_win.exe -ldflags "-X main.Buildstamp=$buildstamp -X main.Hash=$hash  -X main.Version=$version" ${PROJECT_NAME}
-
-fi
-
-
-
-
-#echo Compiling for Linux...
-#export GOARCH=amd64
-#export GOOS=linux
-#go build -o /usr/local/bin/${PROJECT_NAME}_linux -ldflags "-X main.Buildstamp=$buildstamp -X main.Hash=$hash  -X main.Version=$version" ${PROJECT_NAME}
-
-#echo Compiling for Mac...
-#export GOARCH=amd64
-#export GOOS=darwin
-#go build -o /usr/local/bin/${PROJECT_NAME}_mac -ldflags "-X main.Buildstamp=$buildstamp -X main.Hash=$hash  -X main.Version=$version" ${PROJECT_NAME}
+npm run f
+npm run s
