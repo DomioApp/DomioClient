0f6e03b27cb76911f057355efa275bd92f0f4973
Sergei Basharov <sergey@basharov.net>
2017-02-05 20:34:00 +0200

    deploy paths fixes

--- deploy/copy_dart_files.sh
+++ deploy/copy_dart_files.sh
@@ -4,6 +4,6 @@ set -e
 echo Copying Dart source files...
 
 rm -rf /usr/local/domio_public/dart
-yes | mv -f ~/domioclient/src/app /usr/local/domio_public
+yes | mv -f ~/domioclient/src/app /usr/local/domio_client
 
 echo Dart source files copied!
