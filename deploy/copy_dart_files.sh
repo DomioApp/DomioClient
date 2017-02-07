ce2d73b898f3da6895864cdd6f93d721aed5b2f1
Sergei Basharov <sergey@basharov.net>
2017-02-07 20:58:15 +0200

    copy config.dart to deployed folder

--- deploy/copy_dart_files.sh
+++ deploy/copy_dart_files.sh
@@ -5,5 +5,6 @@ echo Copying Dart source files...
 
 rm -rf /usr/local/domio_client/app
 yes | mv -f ~/domioclient/src/app /usr/local/domio_client
+yes | mv -f /usr/local/domio_client/app/config.dart.sample /usr/local/domio_client/app/config.dart
 
 echo Dart source files copied!
