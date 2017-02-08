7b89f9da171311e65fef3c1f52cd0c017579b8cd
Sergei Basharov <sergey@basharov.net>
2017-02-08 13:25:37 +0200

    added dart to deploy process

--- deploy/install_dart.sh
+++ deploy/install_dart.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 echo Installing Dart...
+
+sudo apt-get install -y apt-transport-https
+sudo sh -c 'curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sudo sh -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+
+sudo apt-get update
 sudo apt-get install -y dart
+
 echo Dart is installed!
 dart --version
