886d56fa1d98e4d265332dd8513efaa7f35c4a18
Sergei Basharov <sergey@basharov.net>
2017-02-10 21:31:41 +0200

    Styling mobile version

--- src/app/app.dart
+++ src/app/app.dart
@@ -1,4 +1,5 @@
 import 'utils.dart';
+import 'dart:html';
 
 import 'add_domain_page.dart';
 import 'login_page.dart';
@@ -7,9 +8,15 @@ import 'profile_page.dart';
 import 'user_domains_page.dart';
 import 'domain_edit_page.dart';
 
+ButtonElement toggleMenuButton;
+DivElement menu;
+
 main() {
     var pageName = getPageName();
+
     var page;
+    bindElements();
+    bindEvents();
 
     switch (pageName) {
         case 'LoginPage':
@@ -40,4 +47,17 @@ main() {
             print('Page is not initialized');
             break;
     }
+}
+
+bindElements() {
+    menu = querySelector('.b-top-bar-container');
+    toggleMenuButton = menu.querySelector('.toggle-menu-button');
+}
+
+void bindEvents() {
+    toggleMenuButton.onClick.listen(toggleMenu);
+}
+
+void toggleMenu(MouseEvent event) {
+    menu.classes.toggle('visible');
 }
