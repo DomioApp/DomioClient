076bb50bca65a1e4f3c83fa4ef60da426ce4bf75
Sergey Basharov <sbasharov@spotware.com>
2017-02-10 11:21:57 +0200

    dart build process

--- deploy/copy_dart_files.sh
+++ deploy/copy_dart_files.sh
@@ -4,7 +4,7 @@ set -e
 echo Copying Dart source files...
 
 rm -rf /usr/local/domio_client/app
-yes | mv -f ~/domioclient/src/app /usr/local/domio_client
+yes | cp -R ~/domioclient/src/app /usr/local/domio_client
 yes | mv -f /usr/local/domio_client/app/config.dart.sample /usr/local/domio_client/app/config.dart
 
 echo Dart source files copied!
